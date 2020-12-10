b4d576eb86a113d6763d5137483dff62165009ea
Vilija Kontrimaite <vikontrimaite@gmail.com>
2020-12-10 11:22:59 +0200

    solve new exercises

--- positive-sum.js
+++ positive-sum.js
@@ -1,4 +1,5 @@
-/* You get an array of numbers, return the sum of all of the positives ones.
+/* https://www.codewars.com/kata/5715eaedb436cf5606000381
+You get an array of numbers, return the sum of all of the positives ones.
 
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
